e98ce07b44eaa903ffe2a3dfa35f404ecd35dfca
Rasmus Kjeldgaard <rkjeldgaard@Rasmuss-MacBook-Pro-2.local>
2024-01-03 14:41:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'keyboard-close-repro',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
